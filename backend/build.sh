f173aecb793aaa536d6c218544356855495a713d
Arun Kannan <arunkannan1701@gmail.com>
2025-12-03 21:45:45 +0530

    build.sh testing 9

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,9 @@ echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Applying migrations..."
+python manage.py migrate --noinput
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
