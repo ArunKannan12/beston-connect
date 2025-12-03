2903b669ae766c60fc7d192db70dd792cc6f20b4
Arun Kannan <arunkannan1701@gmail.com>
2025-12-03 21:27:16 +0530

    build.sh testing 7

--- backend/build.sh
+++ backend/build.sh
@@ -3,12 +3,15 @@
 set -o errexit
 
 echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Collecting static files..."
+cd backend
 python manage.py collectstatic --noinput
 
 echo "Fixing migration order issue..."
-python fix_migrations.py   # <-- fakes missing dependencies & applies remaining migrations
+python fix_migrations.py
 
+cd ..
 echo "Build completed successfully ✅"
