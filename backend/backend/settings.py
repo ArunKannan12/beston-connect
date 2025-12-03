1b61ef2dfaf82865acd8bec2e661ec1c30108d44
Arun Kannan <arunkannan1701@gmail.com>
2025-12-03 20:53:10 +0530

    build.sh testing 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -337,8 +337,7 @@ SOCIAL_AUTH_PIPELINE = [
 
 
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 CORS_ALLOW_CREDENTIALS = True
