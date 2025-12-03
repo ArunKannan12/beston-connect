14b0cff655b3f0ba4451727ea9fbe616869c1502
Arun Kannan <arunkannan1701@gmail.com>
2025-12-03 19:47:17 +0530

    Update render.yaml and build.sh for Render deployment

--- backend/build.sh
+++ backend/build.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Setting Django settings module..."
-export DJANGO_SETTINGS_MODULE=backend.settings
+export DJANGO_SETTINGS_MODULE=backend.backend.settings
 
 echo "Collecting static files..."
-cd backend          # <-- REQUIRED because manage.py is inside backend/
+cd backend
 python manage.py collectstatic --noinput
 cd ..
 
