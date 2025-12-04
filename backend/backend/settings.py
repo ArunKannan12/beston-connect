36561643628b71c79996a18bbda8c3e4ea1ea7a8
Arun Kannan <arunkannan1701@gmail.com>
2025-12-04 16:01:25 +0530

    render.yaml and build.sh solution testing 13

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,22 +137,15 @@ if ENVIRONMENT == 'production':
             "DATABASE_URL is not set or invalid! Check Render environment variables."
         )
 else:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql',
-    #         'NAME': os.getenv('DB_NAME'),
-    #         'USER': os.getenv('DB_USER'),
-    #         'PASSWORD': os.getenv('DB_PASSWORD'),
-    #         'HOST': os.getenv('DB_HOST'),
-    #         'PORT': os.getenv('DB_PORT'),
-    #     }
-    # }
     DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
     }
 
 # Optional: print parsed database settings for debugging in Render logs
